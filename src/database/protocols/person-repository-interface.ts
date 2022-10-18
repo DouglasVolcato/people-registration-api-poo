137500da8fae8f0ac7d07469b19caac436e04e69
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-18 02:59:40 -0300

    refactor: remove unused files

--- src/database/protocols/person-repository-interface.ts
+++
@@ -1,9 +0,0 @@
-import { Person } from "./person-interface";
-
-export interface PersonRepositoryInterface {
-  create(body: Person): Person;
-  delete(id: string): Person;
-  getAll(): Person[];
-  get(id: string): Person;
-  update(id: string, body: Person): Person;
-}
