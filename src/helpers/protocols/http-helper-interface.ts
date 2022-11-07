1d203e7814c0132b5a1fca1e53375c512bce4bd0
Douglas Volcato <douglasvolcato@gmail.com>
2022-11-07 19:46:57 -0300

    fix: fix type

--- src/helpers/protocols/http-helper-interface.ts
+++ src/helpers/protocols/http-helper-interface.ts
@@ -2,6 +2,6 @@ import { HttpResponse } from "../../protocols/http";
 import { Person } from "../../protocols/person-interface";
 
 export interface HttpHelpersInterface {
-  badRequest(error: Error): HttpResponse;
-  ok(data: Person | Person[]): HttpResponse;
+  badRequest(error: Error | unknown): HttpResponse;
+  ok(data: Person | Person[] | unknown): HttpResponse;
 }
